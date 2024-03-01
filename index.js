202fce4563cc7533b86cc282c0bcb66d7bba5362
Coding Hub <n.jm90@yahoo.com>
2024-03-01 22:57:01 +0300

    1st push

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-function displaySongInfo() {
 
+function displaySongInfo(event) {
+    event.preventDefault();
+    document.getElementById("mainForm").classList.add("hide");
+    document.getElementById("showSharedSong").classList.remove("hide")
+     const formData = document.getElementById("songForm")
+     const songTitle = formData.elements["title"].value
+     const artist = formData.elements["artist"].value
+     const link = formData.elements["songLink"].value
+     const yearListen = formData.elements["year"].value
+     const desc = formData.elements["description"].value
+
+     document.getElementById("video").setAttribute("src", link)
+     document.querySelector("#para1").innerHTML += songTitle
+     document.querySelector("#para2").innerHTML += artist
+     document.querySelector("#para3").innerHTML += yearListen
+     document.querySelector("#para4").innerHTML += desc
+
+
+
+   
+    /*document.getElementById("para2").innerHTML = "Hey Love it"*/
 }
+
 module.exports = {
     displaySongInfo: displaySongInfo,
 };
+
+
+/*aliceBlue*/
